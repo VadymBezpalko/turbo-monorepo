e81910d623a0e9121fc10f43c426557076ea00f0
Vadym <v.bezpalko@livechat.com>
2024-11-20 13:13:21 +0100

    chore: renaming packages

--- packages/product-switcher/tsup.config.ts
+++ packages/product-switcher/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
+  entry: ["src/button.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
